311d75f356c84b00ee5405396285c3b7dfa82894
Jose Douglas Ramirez E <chibchombiano26@hotmail.com>
2015-05-20 18:05:39 -0500

    Inicio vistas

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -2,18 +2,54 @@ angular
   .module('odontologiaApp').config(['$stateProvider', '$urlRouterProvider', 
   	function ($stateProvider, $urlRouterProvider) {
 
-  		$urlRouterProvider.otherwise('/');
+  		$urlRouterProvider.otherwise('/login');
 
   		$stateProvider
 
+/*
   		.state('main',{
   			url: "/",
   			templateUrl : "/views/main.html"
   		})
+  */
 
-  		.state('register',{
-  			url: "/register",
-  			templateUrl : "/views/register/register.html"
-  		})
+      .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "/views/Menus/Principal.html"    
+      })
+
+      .state('app.main', {
+        url: "/main",
+        cache: false,
+        views: {
+            'menuContent': {
+                templateUrl: "/views/main.html"
+            }       
+            }
+      })
+
+
+      /******************** Auth *******************/
+      .state('register', {
+        url: "/register",
+        cache: false,
+        templateUrl : "/views/register/register.html"        
+      })
+
+      .state('login', {
+        url: "/login",
+        cache: false,
+        templateUrl : "/views/register/login.html"
+      })
+
+      .state('forgot', {
+        url: "/forgot",
+        cache: false,
+        templateUrl : "/views/register/forgot.html"
+      })
+
+      /*********************************************/
+    
   	
   }])
